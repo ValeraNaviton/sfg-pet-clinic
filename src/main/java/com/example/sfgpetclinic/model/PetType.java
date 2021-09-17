ff50796f31b5fb154d10b79352c089c0fb71c015
Valeriu Navitaniuc <valera.navitaniuc@gmail.com>
2021-09-17 00:50:57 -0400

    Model added

---
+++ src/main/java/com/example/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.example.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class PetType {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
+
+
